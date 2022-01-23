6db05894f17b1716a91cf4f2ef0d0174280baca7
rrupapriya <rupapriyaselvam@gmail.com>
2022-01-23 13:29:05 -0500

    c

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -3,11 +3,13 @@ package com.pluralsight.blog.data;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.blog.model.Author;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 
 	
